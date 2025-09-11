c8dcbee0b300379aeefdca037031b66559f9f0e9
marcuscastelo <marcuscastelo@usp.br>
2025-09-11 10:50:03 -0300

    chore(search): remove dead code and comments

--- src/modules/search/application/usecases/cachedSearchCrud.ts
+++ src/modules/search/application/usecases/cachedSearchCrud.ts
@@ -3,17 +3,11 @@ import { createCachedSearchRepository } from '~/modules/search/infrastructure/ca
 const cachedSearchRepository = createCachedSearchRepository()
 
 export async function isSearchCached(query: string): Promise<boolean> {
-  const isCached = await cachedSearchRepository.isSearchCached(query)
-
-  // trackSearchCache removed - using withUserFlowSpan directly now
-
-  return isCached
+  return await cachedSearchRepository.isSearchCached(query)
 }
 
 export async function markSearchAsCached(query: string): Promise<void> {
   await cachedSearchRepository.markSearchAsCached(query)
-
-  // trackSearchCache removed - using withUserFlowSpan directly now
 }
 
 export async function unmarkSearchAsCached(query: string): Promise<void> {
