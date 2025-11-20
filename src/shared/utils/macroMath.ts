f5d5419c3bdee2415e94a0cedb8475d0dfea3e1b
marcuscastelo <marcuscastelo@usp.br>
2025-11-20 02:42:06 -0300

    refactor(meal): introduce MealExt and move meal macro calculation

--- src/shared/utils/macroMath.ts
+++ src/shared/utils/macroMath.ts
@@ -1,15 +1,11 @@
 import { calcDayMacros } from '~/modules/diet/day-diet/application/usecases/dayMacros'
 import { type DayDiet } from '~/modules/diet/day-diet/domain/dayDiet'
 import { MacroNutrientsExt } from '~/modules/diet/macro-nutrients/application/macroExt'
-import { type Meal } from '~/modules/diet/meal/domain/meal'
 import { ItemExt } from '~/modules/diet/unified-item/application/itemExt'
 import { type UnifiedItem } from '~/modules/diet/unified-item/schema/unifiedItemSchema'
 
 export const calcUnifiedItemCalories = (item: UnifiedItem) =>
   MacroNutrientsExt.calories(ItemExt.macros(item))
-
-export const calcMealCalories = (meal: Meal) =>
-  MacroNutrientsExt.calories(ItemExt.calcItemContainerMacros(meal))
 
 export const calcDayCalories = (day: DayDiet) =>
   MacroNutrientsExt.calories(calcDayMacros(day))
