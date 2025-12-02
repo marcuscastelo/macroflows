8f2264e7e6266e11937adcafad4ac3d3698e3d50
marcuscastelo <marcuscastelo@usp.br>
2025-12-02 18:23:05 -0300

    refactor(food): validate macros field in Food DTO and create macro nutrients

--- src/modules/diet/food/infrastructure/api/infrastructure/supabase/supabaseFoodMapper.ts
+++ src/modules/diet/food/infrastructure/api/infrastructure/supabase/supabaseFoodMapper.ts
@@ -3,6 +3,7 @@ import {
   foodSchema,
   type NewFood,
 } from '~/modules/diet/food/domain/food'
+import { createMacroNutrients } from '~/modules/diet/macro-nutrients/domain/macroNutrients'
 import { type Database } from '~/shared/supabase/database.types'
 import { parseWithStack } from '~/shared/utils/parseWithStack'
 
@@ -11,10 +12,27 @@ export type InsertFoodDTO = Database['public']['Tables']['foods']['Insert']
 export type UpdateFoodDTO = Database['public']['Tables']['foods']['Update']
 
 function toDomain(dto: FoodDTO): Food {
+  if (
+    dto.macros === null ||
+    !(typeof dto.macros === 'object') ||
+    !('carbs' in dto.macros) ||
+    !('protein' in dto.macros) ||
+    !('fat' in dto.macros) ||
+    typeof dto.macros.carbs !== 'number' ||
+    typeof dto.macros.protein !== 'number' ||
+    typeof dto.macros.fat !== 'number'
+  ) {
+    throw new Error('Food DTO is missing macros field')
+  }
   return parseWithStack(foodSchema, {
     ...dto,
     ean: dto.ean ?? null,
     source: dto.source ?? undefined,
+    macros: createMacroNutrients({
+      carbsInMg: dto.macros.carbs * 1000,
+      proteinInMg: dto.macros.protein * 1000,
+      fatInMg: dto.macros.fat * 1000,
+    }),
   })
 }
 
