68f55220c5d41dd8c97e1f94d189cfd7965fc7da
marcuscastelo <marcuscastelo@usp.br>
2025-12-05 17:09:40 -0300

    refactor(di): restructure use cases initialization with memoization

--- src/di/useCases.ts
+++ src/di/useCases.ts
@@ -1,17 +1,28 @@
-import { createRoot } from 'solid-js'
+import { createMemo, createRoot } from 'solid-js'
 
 import { createAuthUseCases } from '~/modules/auth/application/usecases/authUseCases'
 import { createUserUseCases } from '~/modules/user/application/usecases/userUseCases'
 import { createGuestUseCases } from '~/shared/guest/guestUseCases'
 
 const container = createRoot(() => {
-  const container = {
-    authUseCases: () => createAuthUseCases(container),
-    userUseCases: () => createUserUseCases(),
-    guestUseCases: () => createGuestUseCases(container),
+  const userUseCasesFactory = () => createUserUseCases()
+  const userUseCases = createMemo(userUseCasesFactory)
+
+  const authUseCasesFactory = () => createAuthUseCases({ userUseCases })
+  const authUseCases = createMemo(authUseCasesFactory)
+
+  const guestUseCasesFactory = () => createGuestUseCases({ authUseCases })
+  const guestUseCases = createMemo(guestUseCasesFactory)
+
+  function container() {
+    return {
+      userUseCases,
+      guestUseCases,
+      authUseCases,
+    }
   }
 
-  return container
+  return container()
 })
 
 export const useCases = {
